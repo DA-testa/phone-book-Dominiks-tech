2c4f8d6e0c1541b276c78e850169f448e2379a0d
221RDB101 Dominiks Vaivods <76108496+Dominiks-tech@users.noreply.github.com>
2023-04-02 20:48:14 +0000

    221RDB101

--- main.py
+++ main.py
@@ -8,42 +8,44 @@ class Query:
             self.name = query[2]
 
 def read_queries():
-    n = int(input())
-    return [Query(input().split()) for i in range(n)]
+    n = input()
+   # return [Query(input().split()) for i in range(n)]
+    query = n.split()
+    kverijs = query[0]
+    numurs = int(query[1])
+    vards = query[2]
+
 
 def write_responses(result):
     print('\n'.join(result))
 
 def process_queries(queries):
-
-# partaisīta linearā meklēšana par hash funkcijas meklēšanu
-
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = {}
+    contacts = []
     for cur_query in queries:
         if cur_query.type == 'add':
             # if we already have contact with such number,
             # we should rewrite contact's name
-            contacts[cur_query.number] = cur_query.name  
-
-
-        
+            for contact in contacts:
+                if contact.number == cur_query.number:
+                    contact.name = cur_query.name
+                    break
+            else: # otherwise, just add it
+                contacts.append(cur_query)
         elif cur_query.type == 'del':
-            if cur_query.number in contacts:
-                del contacts[cur_query.number]
+            for j in range(len(contacts)):
+                if contacts[j].number == cur_query.number:
+                    contacts.pop(j)
+                    break
         else:
-            
-            
-            if cur_query.number not in contacts:
-                response = 'not found'
-                
-            else: 
-                response = contacts[cur_query.number]
-                    
-    result.append(response)
+            response = 'not found'
+            for contact in contacts:
+                if contact.number == cur_query.number:
+                    response = contact.name
+                    break
+            result.append(response)
     return result
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
